ac2ad5b2c992800a5adcdd09a49af488b404128c
Nakkireddy Mahesh <43090741+NMahesh20@users.noreply.github.com>
2020-07-23 21:00:18 +0530

    Update getLikedMusic.js

--- getLikedMusic.js
+++ getLikedMusic.js
@@ -3,10 +3,10 @@ var fs=require("fs");
 
 //First login into spotify and create Developer account
 //Get user id from "https://www.spotify.com/in/account/overview/"
-var user_id="31hvqm6f5uoe236rykh6kps6wj6u";
+var user_id="<your user_id>";
 
 //Get it From https://developer.spotify.com/console/get-current-user-saved-tracks/ click get OAuth token and select user-library-read
-var token="BQB5z7I4glIEcfi6BHVshDqNhUkOZVkmgV99OwETpzZJWfXEgoVjkzBGaM45Sz6QzjeG5Kp2UBqW9gm8wOQE-dEom5KuNHnUVCFXCX7hTIyy4NSsLjnnrGjWi2kJl2kudTzlWVZf69t2CQliXZPkpoEO4IBz8mwqqc1srrK7IzK5FlYal4dlyzXqqQ"
+var token="<Your token>"
 
 var playlists_url="https://api.spotify.com/v1/users/"+user_id+"/tracks?limit=50";
 var writeStream = fs.createWriteStream("./YoutubeLinks.txt",{flags:"a"});
